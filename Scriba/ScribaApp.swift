05e2521d40094bb67e1cf52e000b340ce8b566a7
Francesco Giannicola <francescogiannicola1@gmail.com>
2026-02-26 20:41:07 +0100

    Initial Commit

---
+++ Scriba/ScribaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScribaApp.swift
+//  Scriba
+//
+//  Created by Francesco Giannicola on 26/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ScribaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
